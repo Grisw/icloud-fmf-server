2a1b18fa90b02a8c8cc572ad720ceca2b53a638d
xutao <514335188@qq.com>
2021-10-30 14:34:45 +0800

    Finish icloud auth.

--- src/main/java/coo/lxt/island/server/icloud/fmf/controller/FmfController.java
+++ src/main/java/coo/lxt/island/server/icloud/fmf/controller/FmfController.java
@@ -1,14 +1,15 @@
 package coo.lxt.island.server.icloud.fmf.controller;
 
 import coo.lxt.island.common.exception.ValidateException;
-import coo.lxt.island.common.icloud.vo.ICloudAuthVO;
-import coo.lxt.island.common.icloud.vo.ICloudSessionVO;
+import coo.lxt.island.common.icloud.api.IICloudApi;
+import coo.lxt.island.common.icloud.vo.ICloudAuthWithTokenVO;
+import coo.lxt.island.common.icloud.vo.ICloudHSA2VO;
+import coo.lxt.island.common.icloud.vo.ICloudLoginVO;
+import coo.lxt.island.common.icloud.vo.ICloudTrustSessionVO;
 import coo.lxt.island.server.icloud.fmf.feign.IICloudService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.io.IOException;
 
 @RestController
 @RequestMapping("/fmf")
@@ -18,11 +19,44 @@ public class FmfController {
     @Autowired
     private IICloudService iCloudService;
 
-    @GetMapping("/hello")
+    @GetMapping("/login")
     @ResponseBody
-    public ICloudSessionVO hello(@RequestParam String accountName, @RequestParam(required = false) String password) throws IOException {
+    public IICloudApi.LoginStatus login(@RequestParam String accountName, @RequestParam String password) throws Exception {
         try {
-            return iCloudService.login(new ICloudAuthVO(accountName, password));
+            return iCloudService.login(new ICloudLoginVO(accountName, password));
+        } catch (ValidateException e) {
+            log.error("ILLegal!!", e);
+            throw e;
+        }
+    }
+
+    @GetMapping("/verify/hsa2")
+    @ResponseBody
+    public boolean verifyHSA2(@RequestParam String accountName, @RequestParam String securityCode) throws Exception {
+        try {
+            return iCloudService.verifyHSA2(new ICloudHSA2VO(accountName, securityCode));
+        } catch (ValidateException e) {
+            log.error("ILLegal!!", e);
+            throw e;
+        }
+    }
+
+    @GetMapping("/trust")
+    @ResponseBody
+    public boolean trustSession(@RequestParam String accountName) throws Exception {
+        try {
+            return iCloudService.trustSession(new ICloudTrustSessionVO(accountName));
+        } catch (ValidateException e) {
+            log.error("ILLegal!!", e);
+            throw e;
+        }
+    }
+
+    @GetMapping("/auth")
+    @ResponseBody
+    public boolean authWithToken(@RequestParam String accountName) throws Exception {
+        try {
+            return iCloudService.authWithToken(new ICloudAuthWithTokenVO(accountName));
         } catch (ValidateException e) {
             log.error("ILLegal!!", e);
             throw e;
--- src/main/java/coo/lxt/island/server/icloud/fmf/feign/fallback/ICloudServiceFallbackFactory.java
+++ src/main/java/coo/lxt/island/server/icloud/fmf/feign/fallback/ICloudServiceFallbackFactory.java
@@ -1,6 +1,9 @@
 package coo.lxt.island.server.icloud.fmf.feign.fallback;
 
-import coo.lxt.island.common.icloud.vo.ICloudSessionVO;
+import coo.lxt.island.common.icloud.vo.ICloudAuthWithTokenVO;
+import coo.lxt.island.common.icloud.vo.ICloudHSA2VO;
+import coo.lxt.island.common.icloud.vo.ICloudLoginVO;
+import coo.lxt.island.common.icloud.vo.ICloudTrustSessionVO;
 import coo.lxt.island.server.icloud.fmf.feign.IICloudService;
 import feign.hystrix.FallbackFactory;
 import lombok.extern.slf4j.Slf4j;
@@ -13,6 +16,26 @@ public class ICloudServiceFallbackFactory implements FallbackFactory<IICloudServ
     @Override
     public IICloudService create(Throwable throwable) {
         log.error("Hystrix fallback.", throwable);
-        return authVO -> new ICloudSessionVO();
+        return new IICloudService() {
+            @Override
+            public LoginStatus login(ICloudLoginVO authVO) throws Exception {
+                return LoginStatus.TRY_LATER;
+            }
+
+            @Override
+            public boolean verifyHSA2(ICloudHSA2VO hsa2VO) throws Exception {
+                return false;
+            }
+
+            @Override
+            public boolean authWithToken(ICloudAuthWithTokenVO authWithTokenVO) throws Exception {
+                return false;
+            }
+
+            @Override
+            public boolean trustSession(ICloudTrustSessionVO trustSessionVO) throws Exception {
+                return false;
+            }
+        };
     }
 }
